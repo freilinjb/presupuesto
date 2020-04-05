cc774b72e96ad335910188419c80ac51b5212eb5
freilinjb <freilinjb@gmail.com>
2020-04-05 14:35:52 -0400

    validando input y agregando metodos

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,14 +1,46 @@
-import React,{ Fragment} from 'react'
+import React,{ Fragment, useState} from 'react'
 
-const Pregunta = () => {
+//Se crea loca porque no pasa por los diferentes componentes
+//Definir el state
+
+const Pregunta = (props) => {
+
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, setError ] = useState(false);
+
+    //Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        // console.log(e.target.value); //probando el envio de dato
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e =>{
+
+        e.preventDefault();
+
+        //validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            setError(true);
+            return;
+        }
+
+        //si pasa la validacion
+        setError(false);
+
+    }
+
+    //version corta
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
             <form action="">
                 <div className="form-group">
-                    <input type="number" className="form-control" placeholder="Coloca tu presupuesto"/>
+                    {/* version corta de onChange de una sola lina */}
+                    {/* <input onChange={e=>guardarCantidad(parseInt(e.target.value))} type="number" className="form-control" placeholder="Coloca tu presupuesto"/> */}
+                    <input onChange={definirPresupuesto} type="number" className="form-control" placeholder="Coloca tu presupuesto"/>
                 </div>
-                <button type="submit" className="btn btn-block btn-primary">Definir presupuesto</button>
+                <button onClick={agregarPresupuesto} type="submit" className="btn btn-block btn-primary">Definir presupuesto</button>
             </form>
         </Fragment>
      );
